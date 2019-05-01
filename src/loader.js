dddfd0a9431294e4c5301294e13920aa4d70174b
Konstantin V <mail@k-vinogradov.ru>
2019-05-01 08:54:08 +0700

    Used querySelector and remove the handler of non-OK GET response

--- src/loader.js
+++ src/loader.js
@@ -1,41 +1,29 @@
 import axios from 'axios';
 
-const proxyUrl = url => `https://cors-anywhere.herokuapp.com/${url}`;
-
-const querySingleNode = (node, selector) => {
-  // TODO: querySelectorAll is quite slow. Replace with something
-
-  const nodes = node.querySelectorAll(selector);
-  if (nodes.length !== 1) {
-    throw Error(`Element ${selector} expected to be single (${nodes.length} found)`);
-  }
-  return nodes.item(0);
-};
+const buildProxyUrl = url => `https://cors-anywhere.herokuapp.com/${url}`;
 
 const parse = (xmlString) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(xmlString, 'application/xml');
+  const channel = doc.querySelector('rss>channel');
 
   // TODO: Add checking if error description XML received
 
   const result = {
-    title: querySingleNode(doc, 'rss>channel>title').textContent,
-    description: querySingleNode(doc, 'rss>channel>description').textContent,
-    content: Array.from(doc.querySelectorAll('rss>channel>item').values()).map(item => ({
-      title: querySingleNode(item, 'title').textContent,
-      link: querySingleNode(item, 'link').textContent,
-      description: querySingleNode(item, 'description').textContent,
+    title: channel.querySelector('title').textContent,
+    description: channel.querySelector('description').textContent,
+    content: Array.from(channel.querySelectorAll('item').values()).map(item => ({
+      title: item.querySelector('title').textContent,
+      link: item.querySelector('link').textContent,
+      description: item.querySelector('description').textContent,
     })),
   };
   return result;
 };
 
 const load = (url) => {
-  const wrapped = proxyUrl(url);
-  return axios.get(wrapped).then(({ status, statusText, data }) => {
-    if (status !== 200) throw Error(`Status ${status} (${statusText}) received`);
-    return parse(data);
-  });
+  const proxyUrl = buildProxyUrl(url);
+  return axios.get(proxyUrl).then(({ data }) => parse(data));
 };
 
 export default load;
